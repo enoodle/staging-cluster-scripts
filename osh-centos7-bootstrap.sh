cf364b0c95fe446fa1180b8c2b5ca45baa687f33
Erez Freiberger <efreiber@redhat.com>
2016-10-09 14:55:40 +0300

    adding mtayer-centos7-{9..12}* hosts, removing origin repo

--- osh-centos7-bootstrap.sh
+++ osh-centos7-bootstrap.sh
@@ -1,5 +1,4 @@
 yum install epel-release -y
-curl -o /etc/yum.repos.d/CentOS-OpenShift.repo https://tdawson.fedorapeople.org/centos/CentOS-OpenShift.repo
 yum clean all
 yum upgrade -y
 yum clean all
